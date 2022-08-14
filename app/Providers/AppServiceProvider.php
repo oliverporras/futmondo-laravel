62f3ba6f94d7866772536a483c98f4737a50c842
Oliver <oliverporras@gmail.com>
2022-08-14 11:09:56 +0200

    Gitignore

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,10 +14,6 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         //
-        $this->app->bind('path.public', function(){
-            return '/var/www/vhosts/oliverporras.com/futmondosage.sticker4life.com';
-        });	  
-
     }
 
     /**
