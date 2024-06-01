4ffa38c2ceb727517f4b540f0d6830094423e1b2
Oliver <oliverporras@gmail.com>
2024-06-01 13:58:45 +0200

    Temporada 23-24

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -109,6 +109,7 @@
                                     <li class="nav-item drop-down">
                                         <a href="">{{ __('Otras temporadas') }}</a>
                                         <ul>
+                                            <li><a href="{{ url('/temporada') }}/23">{{ __('Temporada') }} 23-24</a></li>
                                             <li><a href="{{ url('/temporada') }}/22">{{ __('Temporada') }} 22-23</a></li>
                                             <li><a href="{{ url('/temporada') }}/21">{{ __('Temporada') }} 21-22</a></li>
                                             <li><a href="{{ url('/temporada') }}/20">{{ __('Temporada') }} 20-21</a></li>
