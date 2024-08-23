b02d9e71a5fe422e69c9c25924bff3eda1654329
Oliver <oliverporras@gmail.com>
2024-08-23 12:33:38 +0200

    Noop

--- app/Models/Equipo.php
+++ app/Models/Equipo.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Equipo extends Model
 {
     use HasFactory;
-    protected $table = 'vwequipos';
+    protected $table = 'tblequipos24';
 
     //Relacion 1:N
     /*
